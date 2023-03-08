0f8b02f3b37f0ce9e5712d94eb56390dabf173fc
Shiru <lorenz.balzereit@gmail.com>
2023-03-08 16:04:51 +0100

    Corrected spelling mistake in comment

--- src/main/java/my_project/model/Parser.java
+++ src/main/java/my_project/model/Parser.java
@@ -11,7 +11,7 @@ public class Parser {
         /// CODE
     }
     ---> Sowohl "Aufbau" und "Durchlauf" muss es *genau* ein mal in jedem Programm geben
-    ---> erzeugeEssen/Wand/Feld/Huhn können nur in Aufbau aufgerufen werden, geh,drehLinks/Rechts,friss nur in Durchlauf
+    ---> erzeugeEssen/Wand/Feld/Huhn können nur in Aufbau aufgerufen werden, geh, drehLinks/Rechts, friss nur in Durchlauf
             der Code in Aufbau wird sofort ausgeführt, der in Durchlauf danach etwas mit je x Sekunden Pause nach jeder Aktion (würde 0.5 oder so vorschlagen)
 
     /// Befehle ///
