e61f81cfb088321082fcc56bce3cd221488aaaf9
Shiru <lorenz.balzereit@gmail.com>
2023-03-08 15:54:18 +0100

    Added Parser.java and Scanner.java Wrote down the allowed words and syntax in the classes as multiline comment

---
+++ src/main/java/my_project/model/Parser.java
@@ -0,0 +1,26 @@
+package my_project.model;
+
+public class Parser {
+    /* ---------- Syntax: ----------
+
+    /// Parts ///
+    part Aufbau {
+        /// CODE
+    }
+    part Durchlauf {
+        /// CODE
+    }
+    ---> Sowohl "Aufbau" und "Durchlauf" muss es *genau* ein mal in jedem Programm geben
+    ---> erzeugeEssen/Wand/Feld/Huhn können nur in Aufbau aufgerufen werden, geh,drehLinks/Rechts,friss nur in Durchlauf
+            der Code in Aufbau wird sofort ausgeführt, der in Durchlauf danach etwas mit je x Sekunden Pause nach jeder Aktion (würde 0.5 oder so vorschlagen)
+
+    /// Befehle ///
+    erzeugeEssen(ZAHL,ZAHL);
+    erzeugeWand(ZAHL,ZAHL);
+    erzeugeHuhn(ZAHL,ZAHL); (Darf nur ein mal aufgerufen werden)
+    geh(ZAHL); ODER geh; (=geh(1);)
+    drehLinks;
+    drehRechts;
+
+     */
+}
---
+++ src/main/java/my_project/model/Scanner.java
@@ -0,0 +1,35 @@
+package my_project.model;
+
+public class Scanner {
+    /* ---------- Wörter: ----------
+
+    /// Schlüsselwörter | Token name: S-WORT ///
+    function
+    part
+
+    /// Befehle | Token name: BEFEHL ///
+    erzeugeEssen
+    erzeugeWand
+    erzeugeFeld
+    erzeugeHuhn
+    geh
+    drehLinks
+    drehRechts
+    friss
+
+    /// Zahlen (für Koordinaten etc.) | Token name: ZAHL ///
+    [0-9]* < 2,147,483,647
+
+    /// Bezeichner | Token name: BEZEICHNER ///
+    [a-z]*[A-z]*[0-9]* (mindestens ein Buchstabe)
+
+    /// Punktuation | Token name: PUNKTUATION ///
+    {
+    }
+    (
+    )
+    ;
+    ,
+
+     */
+}
