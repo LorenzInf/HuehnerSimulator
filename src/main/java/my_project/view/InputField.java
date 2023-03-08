7537ce2afa1f09253ff61b5d0b2bc2176c091649
Shiru <lorenz.balzereit@gmail.com>
2023-03-08 15:05:51 +0100

    Added InputField Added textArea with getter to InputField.form

---
+++ src/main/java/my_project/view/InputField.java
@@ -0,0 +1,12 @@
+package my_project.view;
+
+import javax.swing.*;
+
+public class InputField {
+    private JTextArea textArea;
+    private JPanel panel1;
+
+    public JTextArea getTextArea() {
+        return textArea;
+    }
+}
