8e7eace919939420c68a76c2693d49ece1ba30cf
Shiru <lorenz.balzereit@gmail.com>
2023-03-09 22:28:24 +0100

    Scanner progress (doesn't fully work yet)

--- src/main/java/my_project/model/Scanner.java
+++ src/main/java/my_project/model/Scanner.java
@@ -1,5 +1,10 @@
 package my_project.model;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Scanner {
     /* ---------- Wörter: ----------
 
@@ -10,7 +15,6 @@ public class Scanner {
     /// Befehle | Token name: BEFEHL ///
     erzeugeEssen
     erzeugeWand
-    erzeugeFeld
     erzeugeHuhn
     geh
     drehLinks
@@ -21,7 +25,7 @@ public class Scanner {
     [0-9]* < 2,147,483,647
 
     /// Bezeichner | Token name: BEZEICHNER ///
-    [a-z]*[A-z]*[0-9]* (mindestens ein Buchstabe)
+    [a-zA-z][a-zA-Z0-9]*
 
     /// Punktuation | Token name: PUNKTUATION ///
     {
@@ -32,4 +36,47 @@ public class Scanner {
     ,
 
      */
+    private KAGO_framework.model.abitur.datenstrukturen.List<Token<String,String>> tokenList;
+
+    public Scanner() {
+
+    }
+
+    public boolean scan(String input) {
+        tokenList = new KAGO_framework.model.abitur.datenstrukturen.List<>();
+        input = input.replaceAll("\t","");
+        String[] inputLines = input.split("\\s");
+        for(String line : inputLines) {
+            for(int i = 0; i < line.length(); i++) { //TODO Fix (Punctuation right after name/number does not get read)
+                if(String.valueOf(line.charAt(i)).matches("[a-zA-z]")) {
+                    StringBuilder currWord = new StringBuilder();
+                    for(; i < line.length() && !String.valueOf(line.charAt(i)).matches("[(){},;\\s]"); i++) {
+                        currWord.append(line.charAt(i));
+                    }
+                    if(currWord.toString().matches("function|part"))
+                        tokenList.append(new Token<>("S-WORD",currWord.toString()));
+                    else if(currWord.toString().matches("erzeugeEssen|erzeugeWand|erzeugeHuhn|geh|drehLinks|drehRechts|friss"))
+                        tokenList.append(new Token<>("BEFEHL",currWord.toString()));
+                    else
+                        tokenList.append(new Token<>("BEZEICHNER",currWord.toString()));
+                } else if(String.valueOf(line.charAt(i)).matches("[0-9]")) {
+                    StringBuilder currWord = new StringBuilder();
+                    for(; i < line.length() && String.valueOf(line.charAt(i)).matches("[0-9]"); i++) {
+                        currWord.append(line.charAt(i));
+                    }
+                    tokenList.append(new Token<>("ZAHL",currWord.toString()));
+                } else if(String.valueOf(line.charAt(i)).matches("[(){},;]")) {
+                    tokenList.append(new Token<>("PUNKTUATION",Character.toString(line.charAt(i))));
+                } else return false;
+            }
+        }
+        //DEBUG
+        tokenList.toFirst();
+        while(tokenList.hasAccess()) {
+            System.out.print("[" + tokenList.getContent().getTokenType() + ", " + tokenList.getContent().getTokenName() + "], ");
+            tokenList.next();
+        }
+        System.out.println();
+        return true;
+    }
 }
--- src/main/java/my_project/view/InputField.java
+++ src/main/java/my_project/view/InputField.java
@@ -1,12 +1,48 @@
 package my_project.view;
 
+import my_project.model.Scanner;
+
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
 
 public class InputField {
     private JTextArea textArea;
     private JPanel panel1;
     private JLabel statusLabel;
     private JButton runButton;
+    private final Scanner scanner;
+
+    public InputField() {
+        scanner = new Scanner();
+        statusLabel.setFont(new Font(statusLabel.getFont().getName(),Font.ITALIC,12));
+        System.out.println(statusLabel.getFont().getSize());
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
+            public void insertUpdate(DocumentEvent e) {
+                scan();
+            }
+            public void removeUpdate(DocumentEvent e) {
+                scan();
+            }
+            public void changedUpdate(DocumentEvent e) {
+
+            }
+        });
+    }
+
+    public void scan() {
+        boolean scanSuccess = scanner.scan(textArea.getText());
+        if(!scanSuccess) {
+            statusLabel.setText("Fix errors");
+            statusLabel.setForeground(new Color(200,10,10));
+            statusLabel.setFont(new Font(statusLabel.getFont().getName(),Font.BOLD,17));
+        } else {
+            statusLabel.setText("No errors");
+            statusLabel.setForeground(Color.BLACK);
+            statusLabel.setFont(new Font(statusLabel.getFont().getName(),Font.ITALIC,12));
+        }
+    }
 
     public JTextArea getTextArea() {
         return textArea;
