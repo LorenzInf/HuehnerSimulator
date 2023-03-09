9aa630411d48b553a79ca06a3e004c9388aa0310
Shiru <lorenz.balzereit@gmail.com>
2023-03-09 19:50:08 +0100

    Deleted InputManager.java Added run button and status label to InputField

--- src/main/java/my_project/view/InputField.java
+++ src/main/java/my_project/view/InputField.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class InputField {
     private JTextArea textArea;
     private JPanel panel1;
+    private JLabel statusLabel;
+    private JButton runButton;
 
     public JTextArea getTextArea() {
         return textArea;
