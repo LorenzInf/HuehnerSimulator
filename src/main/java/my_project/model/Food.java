2eb5d84e914fa2d24e8c45a80c0c89c9b248014c
Furkan <akdafurk@kkg-do.eu>
2023-03-12 23:27:33 +0100

    Hintergrund Bild gefixt

--- src/main/java/my_project/model/Food.java
+++ src/main/java/my_project/model/Food.java
@@ -3,6 +3,6 @@ package my_project.model;
 public class Food extends GameComponent {
 
     public Food() {
-        img = createImage("src/main/resources/graphic/Food.png");
+        img = createImage("src/main/resources/graphic/Futter.png");
     }
 }
